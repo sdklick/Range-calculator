fbd5be70c1c5f9de407311934737bfba8c69cb66
sdklick <72781364+sdklick@users.noreply.github.com>
2021-12-22 13:03:04 +0530

    km

---
+++ km.js
@@ -0,0 +1,12 @@
+function mi(){
+    var a = prompt(" Please Enter The Mile");
+    var b = 1.60934;
+    var c = a*b;
+    return c;
+}
+function km(){
+    var a = prompt("Please Enter The kilometer");
+    var b = 1.60934;
+    var c = a/b;
+    return c;
+}
